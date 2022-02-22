25d2bbb68014bf0bc5067fc826fc4142e7e4496b
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-02-22 13:46:36 +0530

    Create binarysearch.cpp

---
+++ Week 1/binarysearch.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+
+	int t;
+	while(t--)
+	{
+		int c,n,target,mid,l,r;
+		c=1;
+		target=0;
+		cin>>n;
+		vector<int> vec;
+	    for(int i=0;i<n;i++)
+	    {
+			int a;
+			cin>>a;
+			vec.push_back(a);
+		}
+		cin>>target;
+		l=vec[0];
+		r=vec[n-1];
+		while(l<=r)
+		{
+			mid=l+(r-l)/2;
+			if(vec[mid]==target)
+			{
+				cout<<"Present "<<c;
+				c++;
+			}
+			else(vec[mid]<target)
+			l=mid+1;
+			else(vec[med]>target)
+			r=mid-1;
+		}		
+		if(c==0)
+		cout<<"Not Present "<<n/2;;
+		vec.clear();		
+	}	
+	
+	return 0;
+}
