17cdc2b52c3df1f50c41a0f708eaaef6f31f3be6
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-02-22 14:42:50 +0530

    Create jumpsearch.cpp

---
+++ Week 1/jumpsearch.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+  int a,n;
+  vector<int> arr;
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+    cin>>a;
+    arr.push_back(a);    
+  }
+  ...
+  
+}
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--) solve();
+  return 0;
+}
