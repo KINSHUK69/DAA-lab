f7b6bfa2ec85aa3e8660f94555c9596b264fbbe1
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2022-02-22 14:22:29 +0530

    Update linearsearch.cpp

--- Week 1/linearsearch.cpp
+++ Week 1/linearsearch.cpp
@@ -2,6 +2,7 @@
 
 #include<iostream>
 #include<vector>
+#include<iterator>
 using namespace std;
 
 int main()
@@ -13,14 +14,14 @@ int main()
 		int c=1,n,target=0;
 		cin>>n;
 		vector<int> vec;
-	    for(int i=0;i<n;i++)
-	    {
+	        for(int i=0;i<n;i++)
+		{
 			int a;
 			cin>>a;
 			vec.push_back(a);
 		}
 		cin>>target;
-		for(int i=vec.begin();i!=vec.begin();i++)
+		for(int i=0;i<n;i++)
 		{
 			if(vec[i]==target)
 			{
